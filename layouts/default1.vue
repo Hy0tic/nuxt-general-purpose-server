1899a8df3aeb5eac913dc2fec4ffc5adc1d6478b
Hy0tic <vothanhdat278@gmail.com>
2024-08-29 20:30:23 -0500

    add tailwindcss, create pages and default layout

---
+++ layouts/default1.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <NavBar />
+      <Nuxt /> <!-- This renders the page content -->
+    </div>
+</template>
+  
+<script>
+  import NavBar from '/components/NavBar.vue';
+  
+  export default {
+    components: {
+      NavBar,
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Optional: Add any layout-specific styles here */
+</style>
+  
