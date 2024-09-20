01b2e33954a696b6a23c46a77a51f763dd545b06
Hy0tic <vothanhdat278@gmail.com>
2024-09-20 02:28:41 -0500

    update ui for login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,8 @@
 <!--pages/login.vue-->
 <script lang="ts" setup>
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+
 async function login(e: Event) {
 	await $fetch("/api/auth/login", {
 		method: "POST",
@@ -10,14 +13,18 @@ async function login(e: Event) {
 </script>
 
 <template>
-	<h1>Sign in</h1>
-	<form method="post" action="/api/auth/login" @submit.prevent="login">
-		<label htmlFor="username">Username</label>
-		<input name="username" id="username" />
-		<br />
-		<label htmlFor="password">Password</label>
-		<input type="password" name="password" id="password" />
-		<br />
-		<button>Continue</button>
-    </form>
+	<div class="flex justify-center items-center">
+		<div class="rounded-lg w-80 h-80 bg-[rgb(24,24,27)] flex flex-col justify-center items-center mt-48">
+			<h1>Sign in</h1>
+			<form method="post" action="/api/auth/login" @submit.prevent="login" class="m-5 flex flex-col">
+				<label htmlFor="username">Username</label>
+				<InputText name="username" id="username" />
+				<br />
+				<label htmlFor="password">Password</label>
+				<InputText type="password" name="password" id="password" />
+				<br />
+				<Button>Continue</Button>
+			</form>
+		</div>
+	</div>
 </template>
