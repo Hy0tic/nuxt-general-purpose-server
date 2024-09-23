1bbda7165ec7f2280fa11ea5d38dc0b01d07a435
Hy0tic <vothanhdat278@gmail.com>
2024-09-22 23:11:58 -0500

    remove fetch photo call

--- pages/melloPhotoStorage.vue
+++ pages/melloPhotoStorage.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex flex-col justify-center items-center gap-10">
-      <div class="flex flex-wrap justify-center items-center gap-5">
+    <div class="flex flex-col justify-center items-center gap-10 mt-40">
+      <!-- <div class="flex flex-wrap justify-center items-center gap-5">
         <img v-for="(image, index) in images" :key="index" :src="image" class="h-36 w-auto rounded-lg" alt="Random Photo" />
-      </div>
+      </div> -->
       <UploadBox />
     </div>
   </template>
@@ -10,19 +10,19 @@
   <script setup lang="ts">
   import { ref, onMounted } from 'vue';
   import UploadBox from '~/components/UploadBox.vue';
-  import { fetchRandomPhoto } from '~/utils/fetchRandomPhoto'; // Import the function
+  // import { fetchRandomPhoto } from '~/utils/fetchRandomPhoto'; // Import the function
   
-  const images = ref<string[]>([]); // Array to hold image URLs
+  // const images = ref<string[]>([]); // Array to hold image URLs
   
-  const loadImages = async () => {
-    for (let i = 0; i < 3; i++) {
-      const photoUrl = await fetchRandomPhoto(); // Use the imported function
-      console.log(photoUrl)
-      if (photoUrl) {
-        images.value.push(photoUrl); // Add the photo URL to the array
-      }
-    }
-  };
+//   const loadImages = async () => {
+//     for (let i = 0; i < 3; i++) {
+//       const photoUrl = await fetchRandomPhoto(); // Use the imported function
+//       console.log(photoUrl)
+//       if (photoUrl) {
+//         images.value.push(photoUrl); // Add the photo URL to the array
+//       }
+//     }
+//   };
   
   const response = await $fetch("/api/auth/amIauthenticated", {
       method: "GET"
@@ -34,7 +34,7 @@
   }
   
   onMounted(() => {
-    loadImages(); // Load three random images when the component is mounted
+    // loadImages(); // Load three random images when the component is mounted
   });
   </script>
   
