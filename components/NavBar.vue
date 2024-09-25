a01ed0aa8a8da790b6db45fb2c109935168dccb3
Hy0tic <vothanhdat278@gmail.com>
2024-09-24 23:55:38 -0500

    add prettier for tailwind

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,7 @@
 	<nav
 		class="navbar flex justify-around bg-[#333] p-[1rem] text-[rgb(255,255,255)]"
 	>
-		<ul class="list-none flex gap-[1rem]">
+		<ul class="flex list-none gap-[1rem]">
 			<li class="cursor-pointer"><NuxtLink to="/">Home</NuxtLink></li>
 			<li class="cursor-pointer"><NuxtLink to="/about">About</NuxtLink></li>
 			<li class="cursor-pointer"><NuxtLink to="/contact">Contact</NuxtLink></li>
