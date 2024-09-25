9c7c967f1360ae613279a951033f76511a682427
Hy0tic <vothanhdat278@gmail.com>
2024-09-25 02:49:19 -0500

    remove dead code

--- components/ImageEntry.vue
+++ components/ImageEntry.vue
@@ -1,19 +1,18 @@
 <template>
-    <div class="container rounded-md bg-[rgb(30,30,30)]">
+	<div class="container rounded-md bg-[rgb(30,30,30)]">
+		<div class="title">
+			{{ title }}
+		</div>
 
-        <div class="title">
-            {{ title }}
-        </div>
-
-        <img :src="url" alt="photo"/>
-    </div>
+		<img :src="url" alt="photo" />
+	</div>
 </template>
 
 <script setup lang="ts">
 	interface Props {
 		title: string;
 		description: string;
-        url: string;
+		url: string;
 	}
 
 	defineProps<Props>();
--- pages/melloPhotoStorage.vue
+++ pages/melloPhotoStorage.vue
@@ -9,21 +9,7 @@
 </template>
 
 <script setup lang="ts">
-	import { ref, onMounted } from "vue";
 	import UploadBox from "~/components/UploadBox.vue";
-	// import { fetchRandomPhoto } from '~/utils/fetchRandomPhoto'; // Import the function
-
-	// const images = ref<string[]>([]); // Array to hold image URLs
-
-	//   const loadImages = async () => {
-	//     for (let i = 0; i < 3; i++) {
-	//       const photoUrl = await fetchRandomPhoto(); // Use the imported function
-	//       console.log(photoUrl)
-	//       if (photoUrl) {
-	//         images.value.push(photoUrl); // Add the photo URL to the array
-	//       }
-	//     }
-	//   };
 
 	const response = await $fetch("/api/auth/amIauthenticated", {
 		method: "GET"
@@ -32,8 +18,4 @@
 	if (response.fresh !== true && process.client) {
 		navigateTo("/login"); // Adjust the route as necessary
 	}
-
-	onMounted(() => {
-		// loadImages(); // Load three random images when the component is mounted
-	});
 </script>
