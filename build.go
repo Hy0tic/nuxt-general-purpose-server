6f08ea6241f593cd079283df609b86654dace6fe
Hy0tic <vothanhdat278@gmail.com>
2024-09-30 00:21:39 -0500

    space out commands to make file more readable

--- build.go
+++ build.go
@@ -13,16 +13,20 @@ func main() {
 	rnr := sys.Runner().WithEnv(map[string]string{
 		"PKGNAME": "cmdio",
 	})
+	
 	err := rnr.Run("echo", "hello from", rnr.Env("PKGNAME"))
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	if _, err := rnr.Get("true"); err == nil {
 		fmt.Println("true always succeeds")
 	}
+
 	if _, err := rnr.Get("false"); err != nil {
 		fmt.Println("false always fails")
 	}
+	
 	err = cmdio.Pipe(
 		rnr.Command("echo", "pIpEs wOrK tOo"),
 		rnr.Command("tr", "A-Z", "a-z"),
@@ -30,6 +34,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	err = cmdio.Pipe(
 		strings.NewReader("Even When Mixed With Other IO"),
 		rnr.Command("tr", "A-Z", "a-z"),
