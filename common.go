34c54f1a31a2e7d622f39c39f943b71060a2550e
Hy0tic <vothanhdat278@gmail.com>
2024-10-01 01:04:26 -0500

    add comment

--- common.go
+++ common.go
@@ -33,7 +33,7 @@ func installAwsCli(rnr *cmdio.Runner) error {
 }
 
 func useNpmOrPnpm(rnr *cmdio.Runner) string {
-	// GH actions runner do not have pnpm pre installed
+	// GH actions runner come with npm pre installed, but pnpm is used for local development
 	if err := rnr.Run("which", "pnpm"); err == nil {
 		return "pnpm"
 	} else {
