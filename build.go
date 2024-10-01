18d6ef77779a9b725293a370b7af0a46f033e7f8
Hy0tic <vothanhdat278@gmail.com>
2024-09-30 23:29:12 -0500

    update yaml

--- build.go
+++ build.go
@@ -2,59 +2,67 @@ package main
 
 import (
 	"log"
-	"strings"
-
-	"lesiw.io/cmdio"
+	// "lesiw.io/cmdio"
 	"lesiw.io/cmdio/sys"
 )
 
+var rnr = sys.Runner().WithEnv(map[string]string{
+	"PKGNAME": "cmdio",
+})
+
+// const cacheBucket = "awkspace-cmdio-cache-test"
+
 func main() {
-	rnr := sys.Runner().WithEnv(map[string]string{
-		"PKGNAME": "cmdio",
-	})
 	defer rnr.Close();
 	
 	err := rnr.Run("echo", "hello from", rnr.Env("PKGNAME"))
-	if err != nil {
-		log.Fatal(err)
-	}
+	if err != nil { log.Fatal(err) }
+
+	err = rnr.Run("which", "aws")
+	if err != nil { log.Fatal(err) }
     
-	err = rnr.Run("npm", "install")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// TODO: download node_module cache from S3 bucket
+
+	err = rnr.Run("pnpm", "install")
+	if err != nil { log.Fatal(err) }
 
     err = rnr.Run("npx", "prisma", "generate")
-    if err != nil {
-		log.Fatal(err)
-	}
+    if err != nil { log.Fatal(err) }
 
-    err = rnr.Run("npm", "run", "build")
-	if err != nil {
-		log.Fatal(err)
-	}
+    err = rnr.Run("pnpm", "run", "build")
+	if err != nil { log.Fatal(err) }
 
-    // pipe example
-    err = cmdio.Pipe(
-		rnr.Command("echo", "pIpEs wOrK tOo"),
-		rnr.Command("tr", "A-Z", "a-z"),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = cmdio.Pipe(
-		strings.NewReader("Even When Mixed With Other IO"),
-		rnr.Command("tr", "A-Z", "a-z"),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// TODO: update node_module cache in S3 bucket
 
 	err = rnr.Run("echo", "goodbye from", rnr.Env("PKGNAME"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	if err != nil { log.Fatal(err) }
 
 }
+
+
+// func writeCache(dst, src string) {
+//     cmdio.MustPipe(
+//         rnr.Command("tar", "-czf", "-", src+"/"),
+//         rnr.Command(aws(), "s3", "cp", "-",
+//             "s3://"+cacheBucket+"/"+dst+".tar.gz"),
+//     )
+// }
+
+// func readCache(key string) {
+//     cmdio.MustPipe(
+//         rnr.Command(aws(), "s3", "cp",
+//             "s3://"+cacheBucket+"/"+key+".tar.gz", "-"),
+//         rnr.Command("tar", "-xzf", "-"),
+//     )
+// }
+
+// func aws() string {
+//     // TODO: wrap all this in sync.OnceValue
+//     if r, err := rnr.Get("which", "aws"); err == nil {
+//         return r.Out
+//     }
+//     // TODO: install AWS cli
+//     return "/usr/local/bin/aws"
+// }
+
+
