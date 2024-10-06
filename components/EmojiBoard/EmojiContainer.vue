c818975c83a0fa35afdecbc5ca1bd78a3df0747b
Hy0tic <vothanhdat278@gmail.com>
2024-10-06 04:25:28 -0500

    format

--- components/EmojiBoard/EmojiContainer.vue
+++ components/EmojiBoard/EmojiContainer.vue
@@ -1,16 +1,16 @@
 <template>
-    <img 
-        :src="$props.url"
-        alt="emote"
-        width="110"
-        class="rounded-md border-solid border-8 border-transparent hover:border-gray-700"
-    />
+	<img
+		:src="$props.url"
+		alt="emote"
+		width="110"
+		class="rounded-md border-8 border-solid border-transparent hover:border-gray-700"
+	/>
 </template>
 
 <script setup lang="ts">
-    interface props {
-        url: string
-    }
+	interface props {
+		url: string;
+	}
 
-    defineProps<props>()
+	defineProps<props>();
 </script>
