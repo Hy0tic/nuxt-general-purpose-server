8c37e639e73333bbebb1b3a0450c889fbb25aa24
Hy0tic <vothanhdat278@gmail.com>
2024-10-07 02:27:29 -0500

    add sample tooltip

--- components/EmojiBoard/EmojiContainer.vue
+++ components/EmojiBoard/EmojiContainer.vue
@@ -1,10 +1,14 @@
 <template>
-	<img
-		:src="$props.url"
-		alt="emote"
-		width="70"
-		class="m-3 rounded-md border-2 border-solid border-[rgb(90,90,90)] hover:border-[rgb(250,250,250)]"
-	/>
+    <button
+        v-tooltip.top="{ value: 'rollycat', autoHide: false }"
+    >
+        <img
+            :src="$props.url"
+            :alt="$props.name"
+            width="70"
+            class="m-3 rounded-md border-2 border-solid border-[rgb(90,90,90)] hover:border-[rgb(250,250,250)]"
+        />
+    </button>
 </template>
 
 <script setup lang="ts">
